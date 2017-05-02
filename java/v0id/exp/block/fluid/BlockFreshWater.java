0d962f95e7e40b6afca5010759ce2185cff528b4
V0idWa1k3r <v0idwa1k39@gmail.com>
2017-05-03 02:03:36 +0300

    Hopefully fixed the nasty TickNextTick list out of synch exception

--- java/v0id/exp/block/fluid/BlockFreshWater.java
+++ java/v0id/exp/block/fluid/BlockFreshWater.java
@@ -1,5 +1,9 @@
 package v0id.exp.block.fluid;
 
+import java.util.Random;
+
+import javax.annotation.Nonnull;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -39,6 +43,177 @@ public class BlockFreshWater extends BlockFluidFinite implements IWater, IInitia
 		GameRegistry.register(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 		this.setQuantaPerBlock(10);
 	}
+	
+    @Override
+    public void updateTick(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state, @Nonnull Random rand)
+    {
+        boolean changed = false;
+        int quantaRemaining = state.getValue(LEVEL) + 1;
+
+        // Flow vertically if possible
+        int prevRemaining = quantaRemaining;
+        quantaRemaining = tryToFlowVerticallyInto(world, pos, quantaRemaining);
+
+        if (quantaRemaining < 1)
+        {
+            return;
+        }
+        else if (quantaRemaining != prevRemaining)
+        {
+            changed = true;
+            if (quantaRemaining == 1)
+            {
+                world.setBlockState(pos, state.withProperty(LEVEL, quantaRemaining - 1), 3);
+                return;
+            }
+        }
+        else if (quantaRemaining == 1)
+        {
+            return;
+        }
+
+        // Flow out if possible
+        int lowerThan = quantaRemaining - 1;
+        int total = quantaRemaining;
+        int count = 1;
+
+        for (EnumFacing side : EnumFacing.Plane.HORIZONTAL)
+        {
+            BlockPos off = pos.offset(side);
+            if (displaceIfPossible(world, off))
+                world.setBlockToAir(off);
+
+            int quanta = getQuantaValueBelow(world, off, lowerThan);
+            if (quanta >= 0)
+            {
+                count++;
+                total += quanta;
+            }
+        }
+
+        if (count == 1)
+        {
+            if (changed)
+            {
+                world.setBlockState(pos, state.withProperty(LEVEL, quantaRemaining - 1), 3);
+            }
+            return;
+        }
+
+        int each = total / count;
+        int rem = total % count;
+
+        for (EnumFacing side : EnumFacing.Plane.HORIZONTAL)
+        {
+            BlockPos off = pos.offset(side);
+            int quanta = getQuantaValueBelow(world, off, lowerThan);
+            if (quanta >= 0)
+            {
+                int newQuanta = each;
+                if (rem == count || rem > 1 && rand.nextInt(count - rem) != 0)
+                {
+                    ++newQuanta;
+                    --rem;
+                }
+
+                if (newQuanta != quanta)
+                {
+                    if (newQuanta == 0)
+                    {
+                        world.setBlockState(off, Blocks.AIR.getDefaultState(), 2);
+                    }
+                    else
+                    {
+                        world.setBlockState(off, getDefaultState().withProperty(LEVEL, newQuanta - 1), 3);
+                    }
+                    world.scheduleUpdate(off, this, this.tickRate);
+                }
+                --count;
+            }
+        }
+
+        if (rem > 0)
+        {
+            ++each;
+        }
+        world.setBlockState(pos, state.withProperty(LEVEL, each - 1), 2);
+    }
+    
+    @Override
+	public int tryToFlowVerticallyInto(World world, BlockPos pos, int amtToInput)
+    {
+        IBlockState myState = world.getBlockState(pos);
+        BlockPos other = pos.add(0, this.densityDir, 0);
+        if (other.getY() < 0 || other.getY() >= world.getHeight())
+        {
+        	world.setBlockState(pos, Blocks.AIR.getDefaultState(), 2);
+            return 0;
+        }
+
+        int amt = getQuantaValueBelow(world, other, this.quantaPerBlock);
+        if (amt >= 0)
+        {
+            amt += amtToInput;
+            if (amt > this.quantaPerBlock)
+            {
+                world.setBlockState(other, myState.withProperty(LEVEL, this.quantaPerBlock - 1), 3);
+                world.scheduleUpdate(other, this, this.tickRate);
+                return amt - this.quantaPerBlock;
+            }
+            else if (amt > 0)
+            {
+                world.setBlockState(other, myState.withProperty(LEVEL, amt - 1), 3);
+                world.scheduleUpdate(other, this, this.tickRate);
+                world.setBlockState(pos, Blocks.AIR.getDefaultState(), 2);
+                return 0;
+            }
+            return amtToInput;
+        }
+        else
+        {
+            int density_other = getDensity(world, other);
+            if (density_other == Integer.MAX_VALUE)
+            {
+                if (displaceIfPossible(world, other))
+                {
+                    world.setBlockState(other, myState.withProperty(LEVEL, amtToInput - 1), 3);
+                    world.scheduleUpdate(other, this, this.tickRate);
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState(), 2);
+                    return 0;
+                }
+                else
+                {
+                    return amtToInput;
+                }
+            }
+
+            if (this.densityDir < 0)
+            {
+                if (density_other < this.density) // then swap
+                {
+                    IBlockState state = world.getBlockState(other);
+                    world.setBlockState(other, myState.withProperty(LEVEL, amtToInput - 1), 3);
+                    world.setBlockState(pos,   state, 3);
+                    world.scheduleUpdate(other, this, this.tickRate);
+                    world.scheduleUpdate(pos,   state.getBlock(), state.getBlock().tickRate(world));
+                    return 0;
+                }
+            }
+            else
+            {
+                if (density_other > this.density)
+                {
+                    IBlockState state = world.getBlockState(other);
+                    world.setBlockState(other, myState.withProperty(LEVEL, amtToInput - 1), 2);
+                    world.setBlockState(other, state, 2);
+                    world.scheduleUpdate(other, this,  this.tickRate);
+                    world.scheduleUpdate(other, state.getBlock(), state.getBlock().tickRate(world));
+                    return 0;
+                }
+            }
+            return amtToInput;
+        }
+    }
 
 	@Override
 	public boolean isAssociatedBlock(Block other)
