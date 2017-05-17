35bb5961b12657507890ca876d47fc64512a65f6
V0idWa1k3r <v0idwa1k39@gmail.com>
2017-05-17 05:55:44 +0300

    TemperatureRange is now NBTSerializable

--- java/v0id/api/exp/world/TemperatureRange.java
+++ java/v0id/api/exp/world/TemperatureRange.java
@@ -2,7 +2,10 @@ package v0id.api.exp.world;
 
 import java.util.Random;
 
-public class TemperatureRange
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.INBTSerializable;
+
+public class TemperatureRange implements INBTSerializable<NBTTagCompound>
 {
 	public float min;
 	public float max;
@@ -42,5 +45,21 @@ public class TemperatureRange
 	public float getTemperature(Random rand)
 	{
 		return (float) (this.getMin() + rand.nextDouble() * (this.getMax() - this.getMin()));
+	}
+
+	@Override
+	public NBTTagCompound serializeNBT()
+	{
+		NBTTagCompound ret = new NBTTagCompound();
+		ret.setFloat("min", this.min);
+		ret.setFloat("max", this.max);
+		return ret;
+	}
+
+	@Override
+	public void deserializeNBT(NBTTagCompound nbt)
+	{
+		this.min = nbt.getFloat("min");
+		this.max = nbt.getFloat("max");
 	}
 }
