f981b5480a88c65369a30ddabe3d8bbfc58ce25d
V0idWa1k3r <v0idwa1k39@gmail.com>
2017-05-21 08:00:20 +0300

    New world gui will now be set to ExPetrum's world type by default

--- java/v0id/exp/handler/ExPHandlerClient.java
+++ java/v0id/exp/handler/ExPHandlerClient.java
@@ -1,11 +1,17 @@
 package v0id.exp.handler;
 
+import java.lang.reflect.Field;
+
+import net.minecraft.client.gui.GuiCreateWorld;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import v0id.api.core.VoidApi;
+import v0id.api.core.logging.LogLevel;
+import v0id.api.exp.data.ExPMisc;
 import v0id.exp.client.render.hud.PlayerHUDRenderer;
 import v0id.exp.client.render.sky.WorldSkyRenderer;
 import v0id.exp.client.render.sky.WorldWeatherRenderer;
@@ -13,6 +19,34 @@ import v0id.exp.util.WeatherUtils;
 
 public class ExPHandlerClient
 {
+	@SubscribeEvent
+	public void onInitGui(GuiScreenEvent.InitGuiEvent event)
+	{
+		if (event.getGui() instanceof GuiCreateWorld)
+		{
+			try
+			{
+				Field f = null;
+				Field[] allFlds = GuiCreateWorld.class.getDeclaredFields();
+				for (int i = allFlds.length - 1; i >= 0; --i)
+				{
+					if (allFlds[i].getType() == Integer.TYPE)
+					{
+						f = allFlds[i];
+						break;
+					}
+				}
+				
+				f.setAccessible(true);
+				f.set(event.getGui(), ExPMisc.worldTypeExP.getWorldTypeID());
+			}
+			catch (Exception ex)
+			{
+				ExPMisc.modLogger.log(LogLevel.Debug, "Could not reflect GuiCreateWorld!", ex);
+			}
+		}
+	}
+	
 	@SubscribeEvent
 	public void onHudRender(RenderGameOverlayEvent.Pre event)
 	{
