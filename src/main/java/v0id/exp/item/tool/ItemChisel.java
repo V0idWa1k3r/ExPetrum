b1027858687c19e25a6168b4270d46f4bf5e1eaa
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-06-27 23:19:32 +0300

    Added chisels.

---
+++ src/main/java/v0id/exp/item/tool/ItemChisel.java
@@ -0,0 +1,81 @@
+package v0id.exp.item.tool;
+
+import net.minecraft.item.ItemStack;
+import org.apache.commons.lang3.tuple.Pair;
+import v0id.api.exp.combat.EnumWeaponWeight;
+import v0id.api.exp.combat.IWeapon;
+import v0id.api.exp.combat.WeaponType;
+import v0id.api.exp.data.ExPCreativeTabs;
+import v0id.api.exp.data.ExPRegistryNames;
+import v0id.api.exp.data.IOreDictEntry;
+import v0id.api.exp.inventory.IWeightProvider;
+import v0id.api.exp.metal.EnumToolClass;
+import v0id.exp.item.IInitializableItem;
+
+public class ItemChisel extends ItemExPWeapon implements IWeapon, IWeightProvider, IInitializableItem, IOreDictEntry
+{
+	public ItemChisel()
+	{
+		super();
+		this.initItem();
+	}
+
+	@Override
+	public void registerOreDictNames()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initItem()
+	{
+		this.setRegistryName(ExPRegistryNames.asLocation(ExPRegistryNames.itemChisel));
+		this.setUnlocalizedName(this.getRegistryName().toString().replace(':', '.'));
+		this.setCreativeTab(ExPCreativeTabs.tabTools);
+		this.setHasSubtypes(true);
+	}
+
+	@Override
+	public float provideWeight(ItemStack item)
+	{
+		return this.getStats(item).getWeight() * this.getToolClass().getWeight();
+	}
+
+	@Override
+	public Pair<Byte, Byte> provideVolume(ItemStack item)
+	{
+		return IWeightProvider.DEFAULT_VOLUME;
+	}
+
+	@Override
+	public WeaponType getType(ItemStack is)
+	{
+		return WeaponType.NONE;
+	}
+
+	@Override
+	public EnumWeaponWeight getWeaponWeight(ItemStack is)
+	{
+		return EnumWeaponWeight.LIGHT;
+	}
+
+	@Override
+	public EnumToolClass getToolClass()
+	{
+		return EnumToolClass.CHISEL;
+	}
+
+	@Override
+	public float getAttackDamage(ItemStack is)
+	{
+		return this.getStats(is).getDamage() * this.getStats(is).getWeaponDamageMultiplier() * 0.3F;
+	}
+
+	@Override
+	public float getAttackSpeed(ItemStack is)
+	{
+		return -1.5F;
+	}
+
+}
