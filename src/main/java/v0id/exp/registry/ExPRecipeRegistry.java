0373a07f97431ccd2c72f258ee145bb4235c623d
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-06-29 09:20:10 +0300

    Added chisel and bits compatibility.

--- src/main/java/v0id/exp/registry/ExPRecipeRegistry.java
+++ src/main/java/v0id/exp/registry/ExPRecipeRegistry.java
@@ -1,10 +1,14 @@
 package v0id.exp.registry;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
+import net.minecraftforge.registries.ForgeRegistry;
 import v0id.api.exp.data.ExPItems;
 import v0id.api.exp.metal.EnumMetal;
 import v0id.api.exp.metal.EnumToolClass;
@@ -14,6 +18,13 @@ import v0id.exp.item.ItemToolHead;
 public class ExPRecipeRegistry extends AbstractRegistry
 {
     public static ExPRecipeRegistry instance;
+
+    @GameRegistry.ObjectHolder("chiselsandbits:chisel_stone")
+    private static final Item chiselStone = null;
+
+    @GameRegistry.ObjectHolder("chiselsandbits:chisel_iron")
+    private static final Item chiselIron = null;
+
     public ExPRecipeRegistry()
     {
         super();
@@ -31,6 +42,19 @@ public class ExPRecipeRegistry extends AbstractRegistry
         for (EnumToolClass toolClass : EnumToolClass.values())
         {
             event.getRegistry().register(new ShapelessOreRecipe(mcloc, ItemToolHead.createToolHead(toolClass, EnumToolStats.COPPER), new ItemStack(ExPItems.moldTool, 1, EnumToolClass.values().length * 2 + toolClass.ordinal())).setRegistryName("exp:recipe_hardcoded_toolhead_" + toolClass.name().toLowerCase()));
+        }
+
+        ForgeRegistry<IRecipe> reg = (ForgeRegistry<IRecipe>) event.getRegistry();
+        if (Loader.isModLoaded("chiselsandbits"))
+        {
+            ResourceLocation[] toRemove = new ResourceLocation[]{ new ResourceLocation("chiselsandbits", "chisel_stone"), new ResourceLocation("chiselsandbits", "chisel_iron"), new ResourceLocation("chiselsandbits", "chisel_gold"), new ResourceLocation("chiselsandbits", "chisel_diamond") };
+            for (ResourceLocation loc : toRemove)
+            {
+                reg.remove(loc);
+            }
+
+            event.getRegistry().register(new ShapelessOreRecipe(mcloc, new ItemStack(chiselStone, 1, 0), new ItemStack(ExPItems.chisel, 1, 0)).setRegistryName("exp:recipe_hardcoded_compat_cnb_chisel_stone"));
+            event.getRegistry().register(new ShapelessOreRecipe(mcloc, new ItemStack(chiselIron, 1, 0), new ItemStack(ExPItems.chisel, 1, 7)).setRegistryName("exp:recipe_hardcoded_compat_cnb_chisel_iron"));
         }
     }
 }
