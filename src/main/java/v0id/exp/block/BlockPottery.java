86697933ae4f982e6a105ce4303caaa15cae8ff5
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-06-30 20:04:48 +0300

    Fixed pottery drops

--- src/main/java/v0id/exp/block/BlockPottery.java
+++ src/main/java/v0id/exp/block/BlockPottery.java
@@ -7,13 +7,14 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -84,13 +85,7 @@ public class BlockPottery extends Block implements IInitializableBlock
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune)
     {
-        return ExPItems.pottery;
-    }
-
-    @Override
-    public int damageDropped(IBlockState state)
-    {
-        return 3 + state.getValue(ExPBlockProperties.POTTERY_TYPE).ordinal();
+        return Items.AIR;
     }
 
     @SuppressWarnings("deprecation")
@@ -160,25 +155,23 @@ public class BlockPottery extends Block implements IInitializableBlock
         return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
     }
 
-    @Override
-    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
     {
-        super.getDrops(drops, world, pos, state, fortune);
-        for (ItemStack is : drops)
+        ItemStack is = new ItemStack(ExPItems.pottery, 1, 3 + state.getValue(ExPBlockProperties.POTTERY_TYPE).ordinal());
+        if (is.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
         {
-            if (is.getItem() == ExPItems.pottery && is.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
+            IItemHandler itemCap = is.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+            TilePot pot = (TilePot)worldIn.getTileEntity(pos);
+            if (pot != null)
             {
-                IItemHandler itemCap = is.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-                TilePot pot = (TilePot)world.getTileEntity(pos);
-                if (pot != null)
+                IItemHandler potCap = pot.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+                for (int i = 0; i < itemCap.getSlots() && i < potCap.getSlots(); ++i)
                 {
-                    IItemHandler potCap = pot.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-                    for (int i = 0; i < itemCap.getSlots() && i < potCap.getSlots(); ++i)
-                    {
-                        itemCap.insertItem(i, potCap.getStackInSlot(i), false);
-                    }
+                    itemCap.insertItem(i, potCap.getStackInSlot(i), false);
                 }
             }
         }
+
+        InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), is);
     }
 }
