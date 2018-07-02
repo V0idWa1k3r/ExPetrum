38342c6013bb776a4f6f9b16b6f104e6e37bbc20
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-07-02 23:43:33 +0300

    More progress is now needed to smith tools.

--- src/main/java/v0id/api/exp/metal/EnumToolClass.java
+++ src/main/java/v0id/api/exp/metal/EnumToolClass.java
@@ -4,20 +4,20 @@ import net.minecraft.util.IStringSerializable;
 
 public enum EnumToolClass implements IStringSerializable
 {
-	PICKAXE("pickaxe", 1.5F, EnumAnvilRequirement.INGOT, 60),
-	AXE("axe", 1.2F, EnumAnvilRequirement.INGOT, 60),
-	SHOVEL("spade", 1.4F, EnumAnvilRequirement.INGOT, 60),
-	HOE("hoe", 1.0F, EnumAnvilRequirement.INGOT, 50),
-	SWORD("sword", 1.0F, EnumAnvilRequirement.SHEET, 50),
-	KNIFE("knife", 0.6F, EnumAnvilRequirement.INGOT, 20),
-	SCYTHE("scythe", 1.2F, EnumAnvilRequirement.SHEET, 70),
-	BATTLEAXE("axe", 1.5F, EnumAnvilRequirement.DOUBLE_SHEET, 80),
-	HAMMER("hammer", 1.8F, EnumAnvilRequirement.DOUBLE_SHEET, 50),
-	SPEAR("sword", 1.0F, EnumAnvilRequirement.INGOT, 40),
-	WATERING_CAN("watering_can", 0.2F, EnumAnvilRequirement.SHEET, 70),
-	GARDENING_SPADE("spade", 0.1F, EnumAnvilRequirement.INGOT, 40),
-	SAW("saw", 0.5F, EnumAnvilRequirement.SHEET, 40),
-    CHISEL("chisel", 0.1F, EnumAnvilRequirement.INGOT, 30);
+	PICKAXE("pickaxe", 1.5F, EnumAnvilRequirement.INGOT, 120),
+	AXE("axe", 1.2F, EnumAnvilRequirement.INGOT, 120),
+	SHOVEL("spade", 1.4F, EnumAnvilRequirement.INGOT, 120),
+	HOE("hoe", 1.0F, EnumAnvilRequirement.INGOT, 100),
+	SWORD("sword", 1.0F, EnumAnvilRequirement.SHEET, 100),
+	KNIFE("knife", 0.6F, EnumAnvilRequirement.INGOT, 40),
+	SCYTHE("scythe", 1.2F, EnumAnvilRequirement.SHEET, 140),
+	BATTLEAXE("axe", 1.5F, EnumAnvilRequirement.DOUBLE_SHEET, 160),
+	HAMMER("hammer", 1.8F, EnumAnvilRequirement.DOUBLE_SHEET, 100),
+	SPEAR("sword", 1.0F, EnumAnvilRequirement.INGOT, 80),
+	WATERING_CAN("watering_can", 0.2F, EnumAnvilRequirement.SHEET, 140),
+	GARDENING_SPADE("spade", 0.1F, EnumAnvilRequirement.INGOT, 140),
+	SAW("saw", 0.5F, EnumAnvilRequirement.SHEET, 80),
+    CHISEL("chisel", 0.1F, EnumAnvilRequirement.INGOT, 60);
 
 	EnumToolClass(String s, float f, EnumAnvilRequirement ear, int i)
 	{
