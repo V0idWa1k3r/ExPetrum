49d147fa82bb9344a1be1eb4495276cca8764335
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-07-13 19:16:28 +0300

    Crates can now be rotated.

--- src/main/java/v0id/exp/client/render/tile/TESRCrate.java
+++ src/main/java/v0id/exp/client/render/tile/TESRCrate.java
@@ -3,6 +3,7 @@ package v0id.exp.client.render.tile;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.client.model.animation.FastTESR;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
@@ -18,9 +19,39 @@ public class TESRCrate extends FastTESR<TileCrate>
         TextureAtlasSprite sprite = Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getTexture(te.getWorld().getBlockState(te.getPos()).getActualState(te.getWorld(), te.getPos()));
         Vector3f pos = new Vector3f((float)x + 0.5F, (float)y + 0.96875F, (float)z);
         Matrix4f transform = Matrix4f.setIdentity(new Matrix4f());
-        transform = transform.rotate((float)Math.toRadians((te.lidAngle + (te.lidAngle - te.prevLidAngle) * partialTicks) * -45F), new Vector3f(1, 0, 0));
-        transform = transform.translate(new Vector3f(0, 0, 0.5F));
-        transform = transform.scale(new Vector3f(1, 0.0625F, 1));
+        EnumFacing facing = te.direction;
+        if (facing == EnumFacing.NORTH)
+        {
+            transform = transform.rotate((float) Math.toRadians((te.lidAngle + (te.lidAngle - te.prevLidAngle) * partialTicks) * -45F), new Vector3f(1, 0, 0));
+            transform = transform.translate(new Vector3f(0, 0, 0.5F));
+            transform = transform.scale(new Vector3f(1, 0.0625F, 1));
+        }
+
+        if (facing == EnumFacing.SOUTH)
+        {
+            transform = transform.translate(new Vector3f(0, 0, 1F));
+            transform = transform.rotate((float) Math.toRadians((te.lidAngle + (te.lidAngle - te.prevLidAngle) * partialTicks) * 45F), new Vector3f(1, 0, 0));
+            transform = transform.rotate((float)Math.PI, new Vector3f(0, 1, 0));
+            transform = transform.translate(new Vector3f(0, 0, 0.5F));
+            transform = transform.scale(new Vector3f(1, 0.0625F, 1));
+        }
+
+        if (facing == EnumFacing.EAST)
+        {
+            transform = transform.translate(new Vector3f(0.5F, 0, 0.5F));
+            transform = transform.rotate((float) Math.toRadians((te.lidAngle + (te.lidAngle - te.prevLidAngle) * partialTicks) * -45F), new Vector3f(0, 0, 1));
+            transform = transform.translate(new Vector3f(-0.5F, 0, 0));
+            transform = transform.scale(new Vector3f(1, 0.0625F, 1));
+        }
+
+        if (facing == EnumFacing.WEST)
+        {
+            transform = transform.translate(new Vector3f(-0.5F, 0, 0.5F));
+            transform = transform.rotate((float) Math.toRadians((te.lidAngle + (te.lidAngle - te.prevLidAngle) * partialTicks) * 45F), new Vector3f(0, 0, 1));
+            transform = transform.translate(new Vector3f(0.5F, 0, 0));
+            transform = transform.scale(new Vector3f(1, 0.0625F, 1));
+        }
+
         int i = te.getWorld().getCombinedLight(te.getPos(), 0);
         int j = i % 65536;
         int k = i / 65536;
